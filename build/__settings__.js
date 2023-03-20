45e8f0b874da7823235c99bc37894fe4e637c493
Pengcheng <5285226+pec017@users.noreply.github.com>
2023-03-20 09:17:11 +0000

    Init commit

---
+++ build/__settings__.js
@@ -0,0 +1,23 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1555690.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+window.SCRIPTS = [ 105425471, 105425450, 105425475, 105425478, 105425484, 105425347, 105425397, 105425494, 105425449, 105425497, 105425343, 105425382, 105425398, 105425206, 105425197, 105425200, 105425437, 105425333, 105425472, 105425435, 105425473, 105425441, 105425214, 105425233, 105425458, 105425526, 105425514, 105425191, 105425456, 105425443, 105425585 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/105425455/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/105425303/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/105425448/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/105425440/1/basis.wasm.js', 'wasmUrl' : 'files/assets/105425226/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/105425527/1/basis.js', 'preload' : false},
+];
